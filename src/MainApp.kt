ce8fd56089535bde31e7c8e10dc35a981c5800ce
genta <gentaindianaluthfi@gmail.com>
2026-04-13 16:25:53 +0700

    Contoh 1 Hello wolrd

--- src/MainApp.kt
+++ src/MainApp.kt
@@ -1,19 +1,3 @@
-fun main() {
-    println("=== Program Kalkulator Sederhana ===")
-    print("Masukkan angka pertama: ")
-    val a = readLine()!!.toDouble()
-
-    print("Masukkan angka kedua: ")
-    val b = readLine()!!.toDouble()
-
-    val tambah = a + b
-    val kurang = a - b
-    val kali = a * b
-    val bagi = a / b
-
-    println("\n--- Hasil Perhitungan ---")
-    println("Hasil Tambah: $tambah")
-    println("Hasil Kurang: $kurang")
-    println("Hasil Kali   : $kali")
-    println("Hasil Bagi   : $bagi")
+fun main(args: Array<String>) {
+    println("Hello, Ini Program Pertama Saya di Kotlin!")
 }
